f1b916768ef7bf06e0a708d2686283660a291bc7
binaryArrow <cancihan96@gmail.com>
2026-02-16 02:22:13 +0100

    fix levels path

--- singletons/level-manager.gd
+++ singletons/level-manager.gd
@@ -1,16 +1,16 @@
 extends Node
 
-const LEVEL_PATH_TEMPLATE := "res://scenes/level/level%d.tscn"
+const LEVEL_PATH_TEMPLATE := "res://scenes/levels/level%d.tscn"
 var current_level := 0
 
 func next_level():
 	current_level += 1
 	var path := LEVEL_PATH_TEMPLATE % current_level
+	print("Loading level: ", path)
 	if ResourceLoader.exists(path):
 		change_scene_to(path)
 	else:
 		push_warning("No more levels — level %d not found." % current_level)
-		current_level -= 1
 
 func change_scene_to(scene_path: String):
 	get_tree().change_scene_to_file(scene_path)
